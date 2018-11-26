7d2b3307e7840c2a5cc3e362fb79aba2302e5ae6
Juan Ovalle <ovallejuan98@gmail.com>
2018-11-25 18:20:33 -0600

    Resive notificaciones de Firebase

--- app/src/main/java/com/example/ovall/practia_iii/Bienvenido.java
+++ app/src/main/java/com/example/ovall/practia_iii/Bienvenido.java
@@ -1,13 +1,33 @@
 package com.example.ovall.practia_iii;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Bienvenido extends AppCompatActivity {
+
+    private BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenido);
+
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                //tVToken.setText(SharedPrefManager.getInstance(MainActivity.this).getToken());
+            }
+        };
+
+        if(SharedPrefManager.getInstance(Bienvenido.this).getToken() != null){
+            Log.d("TAG", SharedPrefManager.getInstance(this).getToken());
+        }
+        registerReceiver(broadcastReceiver, new IntentFilter(MyFirebaseInstanceIDService.TOKEN_BROADCAST));
+
     }
 }
--- app/src/main/java/com/example/ovall/practia_iii/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/ovall/practia_iii/MyFirebaseMessagingService.java
@@ -3,11 +3,57 @@ package com.example.ovall.practia_iii;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MyFirebaseMessagingService extends Service {
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
 
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
+        String tag = "El mensaje: ";
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(tag, "From: " + remoteMessage.getFrom());
+        //Toast.makeText(getApplicationContext(), "A "+ remoteMessage.getFrom(), Toast.LENGTH_SHORT).show();
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(tag, "Message data payload: " + remoteMessage.getData());
+          //  Toast.makeText(getApplicationContext(), "B "+ remoteMessage.getData().toString(), Toast.LENGTH_SHORT).show();
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                // scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+                // handleNow();
+            }
+
+        }
+
+        // Check if message contains a notification payload.
+        Log.d(tag, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        //Toast.makeText(getApplicationContext(), "C "+ remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT).show();
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(tag, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            //Toast.makeText(getApplicationContext(), "D "+ remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT).show();
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        notifyUser(remoteMessage.getFrom(), remoteMessage.getNotification().getBody());
+    }
+
+    public void notifyUser(String from, String notification){
+        MyNotificationManager myNotificationManager = new MyNotificationManager(getApplicationContext());
+
+        myNotificationManager.showNotification(from, notification, new Intent(getApplicationContext(), Bienvenido.class));
     }
 }
